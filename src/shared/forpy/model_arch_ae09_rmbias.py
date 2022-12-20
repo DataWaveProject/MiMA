3f2b8a132413f35a91e84413bc674becdd78d4b7
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2022-12-20 10:32:09 +0000

    cleaned up some python (+black&flake8)

--- src/shared/forpy/model_arch_ae09_rmbias.py
+++ src/shared/forpy/model_arch_ae09_rmbias.py
@@ -1,58 +1,119 @@
 # import torch
 import torch.nn as nn
 from torch import zeros, reshape, cat, max, min, tensor
+
 # import torch.nn.functional as F
 # import numpy as np
 import sys
-sys.path.append('/home/lmy7879/gwp/codes')
+
+sys.path.append("/home/lmy7879/gwp/codes")
 # from utils import count_params, init_xavier
 from numpy import reshape, float32, array, searchsorted
 
-# x3 is shaped (batch_size, 80). 
+# x3 is shaped (batch_size, 80).
 # gu is shaped (batch_size, 40).
 def init_xavier(m):
     if type(m) == nn.Conv2d or type(m) == nn.Linear or type(m) == nn.ConvTranspose2d:
         nn.init.xavier_uniform_(m.weight)
 
+
 class model(nn.Module):
-    def __init__(self, s_dict,n_levels=40):
+    def __init__(self, s_dict, n_levels=40):
         super(model, self).__init__()
-        self.n_levels = n_levels # Input size. (length of column)
-        self.n_ch = s_dict['n_ch']
-        self.n_d = s_dict['n_d']
-        self.kernel_size=3
-        self.n_dilations = s_dict['n_dilations']
-        self.lon=s_dict['lon']; self.lat = s_dict['lat'];
-        self.n_flat_in = int(self.n_ch[3]*n_levels/2)+3
-        self.n_flat_out = int(self.n_ch[3]*n_levels/2)
-        self.input_stats = {'mean_3': s_dict['input_3d_mean'], 'std_3': s_dict['input_3d_std'], 
-                            'mean_sp': s_dict['sp_mean'], 'std_sp': s_dict['sp_std']}
-        self.output_stats = {'mean': s_dict['target_mean'], 'std': s_dict['target_std']}
-        self.sm_mean=s_dict['sm_mean'] # shear metric
-        self.source_level = s_dict['source_level']
-        self.bin_edges = s_dict['bin_edges']
+        self.n_levels = n_levels  # Input size. (length of column)
+        self.n_ch = s_dict["n_ch"]
+        self.n_d = s_dict["n_d"]
+        self.kernel_size = 3
+        self.n_dilations = s_dict["n_dilations"]
+        self.lon = s_dict["lon"]
+        self.lat = s_dict["lat"]
+        self.n_flat_in = int(self.n_ch[3] * n_levels / 2) + 3
+        self.n_flat_out = int(self.n_ch[3] * n_levels / 2)
+        self.input_stats = {
+            "mean_3": s_dict["input_3d_mean"],
+            "std_3": s_dict["input_3d_std"],
+            "mean_sp": s_dict["sp_mean"],
+            "std_sp": s_dict["sp_std"],
+        }
+        self.output_stats = {"mean": s_dict["target_mean"], "std": s_dict["target_std"]}
+        self.sm_mean = s_dict["sm_mean"]  # shear metric
+        self.source_level = s_dict["source_level"]
+        self.bin_edges = s_dict["bin_edges"]
         del s_dict
         self.cnn_encode = nn.Sequential(
-            nn.Conv1d(self.n_ch[0], self.n_ch[1], self.kernel_size, 1, padding='same', dilation=self.n_dilations[0]), nn.ELU(),
-            nn.Conv1d(self.n_ch[1], self.n_ch[2], self.kernel_size, 1, padding='same', dilation=self.n_dilations[1]), nn.ELU(),
-            nn.Conv1d(self.n_ch[2], self.n_ch[3], self.kernel_size, 1, padding='same', dilation=self.n_dilations[2]), nn.ELU(),
-            nn.MaxPool1d(kernel_size=2),            nn.Flatten()
+            nn.Conv1d(
+                self.n_ch[0],
+                self.n_ch[1],
+                self.kernel_size,
+                1,
+                padding="same",
+                dilation=self.n_dilations[0],
+            ),
+            nn.ELU(),
+            nn.Conv1d(
+                self.n_ch[1],
+                self.n_ch[2],
+                self.kernel_size,
+                1,
+                padding="same",
+                dilation=self.n_dilations[1],
+            ),
+            nn.ELU(),
+            nn.Conv1d(
+                self.n_ch[2],
+                self.n_ch[3],
+                self.kernel_size,
+                1,
+                padding="same",
+                dilation=self.n_dilations[2],
+            ),
+            nn.ELU(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Flatten(),
         )
         self.dense = nn.Sequential(
-            nn.Linear(self.n_flat_in, self.n_d),       nn.ELU(),
-            nn.Linear(self.n_d,       self.n_d),       nn.ELU(),
+            nn.Linear(self.n_flat_in, self.n_d),
+            nn.ELU(),
+            nn.Linear(self.n_d, self.n_d),
+            nn.ELU(),
             # nn.Linear(self.n_d,    self.n_d), nn.ELU(), nn.BatchNorm1d(100)
             # nn.Linear(self.n_d,    self.n_d), nn.ELU(), nn.BatchNorm1d(100)
-            nn.Linear(self.n_d,       self.n_flat_out), nn.ELU()
-            )
+            nn.Linear(self.n_d, self.n_flat_out),
+            nn.ELU(),
+        )
         self.cnn_decode = nn.Sequential(
-            nn.ConvTranspose1d(self.n_ch[3], self.n_ch[2], self.kernel_size, 2, padding=1, output_padding=1, dilation=self.n_dilations[3]), nn.ELU(),
-            nn.ConvTranspose1d(self.n_ch[2], self.n_ch[1], self.kernel_size, 1, padding=1, dilation=self.n_dilations[4]), nn.ELU(),
-            nn.ConvTranspose1d(self.n_ch[1], 1,          self.kernel_size, 1, padding=1, dilation=self.n_dilations[5])
+            nn.ConvTranspose1d(
+                self.n_ch[3],
+                self.n_ch[2],
+                self.kernel_size,
+                2,
+                padding=1,
+                output_padding=1,
+                dilation=self.n_dilations[3],
+            ),
+            nn.ELU(),
+            nn.ConvTranspose1d(
+                self.n_ch[2],
+                self.n_ch[1],
+                self.kernel_size,
+                1,
+                padding=1,
+                dilation=self.n_dilations[4],
+            ),
+            nn.ELU(),
+            nn.ConvTranspose1d(
+                self.n_ch[1],
+                1,
+                self.kernel_size,
+                1,
+                padding=1,
+                dilation=self.n_dilations[5],
+            ),
         )
         self.cnn_encode.apply(init_xavier)
         self.dense.apply(init_xavier)
         self.cnn_decode.apply(init_xavier)
+
     def forward(self, x):
         # Unpack
         x3, xloc = x
@@ -60,31 +121,45 @@ class model(nn.Module):
         # print(xloc.shape)
         # get latitude info
         batchsize = xloc.shape[0]
-        whichlat=zeros(batchsize,dtype=int)
-        for j,la in enumerate(self.lat.astype(float32)):
-            whichlat[xloc[:,0]==la]=j
-        u_range = tensor(array([max(x3[l,0,:self.source_level[whichlat[l]]])-min(x3[l,0,:self.source_level[whichlat[l]]]) for l in range(batchsize)])); del whichlat
-        i = searchsorted(self.bin_edges, u_range, side='left'); del u_range; 
+        whichlat = zeros(batchsize, dtype=int)
+        for j, la in enumerate(self.lat.astype(float32)):
+            whichlat[xloc[:, 0] == la] = j
+        u_range = tensor(
+            array(
+                [
+                    max(x3[l, 0, : self.source_level[whichlat[l]]])
+                    - min(x3[l, 0, : self.source_level[whichlat[l]]])
+                    for l in range(batchsize)
+                ]
+            )
+        )
+        del whichlat
+        i = searchsorted(self.bin_edges, u_range, side="left")
+        del u_range
 
         # Standardize
-        x3 -= reshape(self.input_stats['mean_3'],(1,4,1)); x3 /= reshape(self.input_stats['std_3'],(1,4,1));
-        xloc[:,-1] -= self.input_stats['mean_sp']; xloc[:,-1]/= self.input_stats['std_sp']
+        x3 -= reshape(self.input_stats["mean_3"], (1, 4, 1))
+        x3 /= reshape(self.input_stats["std_3"], (1, 4, 1))
+        xloc[:, -1] -= self.input_stats["mean_sp"]
+        xloc[:, -1] /= self.input_stats["std_sp"]
         # Encod:,e 3d variables
-        z = self.cnn_encode(x3)#; print(z3.shape)
+        z = self.cnn_encode(x3)  # ; print(z3.shape)
 
         # Concatenate with loc variables
-        z = cat((z,xloc),axis=1)
-        
+        z = cat((z, xloc), axis=1)
+
         # Dense it up.
         z = self.dense(z)
 
         # Reshape for convolutions.
-        z = reshape(z,(z.shape[0], self.n_ch[3], int(self.n_levels/2)))
-        
+        z = reshape(z, (z.shape[0], self.n_ch[3], int(self.n_levels / 2)))
+
         # Decode.
         gu = self.cnn_decode(z).squeeze()
 
         # Unstandardize
-        gu *= self.output_stats['std']; gu += self.output_stats['mean']
-        gu -= self.sm_mean[i,:].astype(float32); del i
+        gu *= self.output_stats["std"]
+        gu += self.output_stats["mean"]
+        gu -= self.sm_mean[i, :].astype(float32)
+        del i
         return gu
--- src/shared/forpy/run_emulator_ae98.py
+++ src/shared/forpy/run_emulator_ae98.py
@@ -1,89 +1,106 @@
-'''
-This python script outlines the forpy commands that will be done in cg_drag_lmy.f90
-In the correct folder, place module named model_arch and the model weights h5 file.
-This additionally removes the bias w.r.t. the shear metric estimated on the training data. 
-
-'''
+"""
+This python script outlines the forpy commands that will be done in
+cg_drag_lmy.f90 In the correct folder, place module named model_arch
+and the model weights h5 file. This additionally removes the bias
+w.r.t. the shear metric estimated on the training data.
+"""
 # Load architecture.
 # import torch
 # import model_arch as m;
 # n_d=[128,64,32,1]; n_in=40; n_out=33
 # device = "cpu"
 # model=m.model(n_d, n_in, n_out).to(device);
-# for branch in model.branches: branch.to(device) 
+# for branch in model.branches: branch.to(device)
 
 # # Load weights and set to evaluation mode.
 # best_model = torch.load(f"model_weights.h5",map_location=torch.device('cpu'))
 # model.load_state_dict(best_model['weights'])
 # model.eval()
 
-# for testing
 import numpy as np
-X3 = np.asfortranarray(np.random.randn(128*2, 4, 40).astype(np.float32))
-X2 = np.asfortranarray(np.random.randn(128*2, 3).astype(np.float32))
-temp = np.random.randn(128*2, 40).astype(np.float32)
-Y_out = np.asfortranarray(np.zeros((128, 2,40), dtype=np.float64))
-del np
-from pickle import dump
-from os.path import exists
-
-from torch import from_numpy, no_grad, reshape, transpose, double
-from numpy import float32, float64, max, min, zeros, array, searchsorted, inf, nan
+# from numpy import float32, float64, max, min, zeros, array, searchsorted, inf, nan
 import pathlib
+from torch import from_numpy, no_grad, reshape, transpose, double
+from torch import load, device
+# Imports required when outputting to MiMA console:
+# from pickle import dump
+# from os.path import exists
+
+
 # Initialize everything
 def initialize():
-    from torch import load, device
-    #import model_arch_ae09_rmbias as m;
-    import model_arch_ae09 as m;
+    # Set to run on CPU
     device_str = "cpu"
-    dirpath = pathlib.Path(__file__).resolve().parent
-    best_model = load(dirpath.joinpath("model_weights_ae98.h5"), map_location=device(device_str))
-    model=m.model(best_model).to(device_str);
 
-    # Load weights and set to evaluation mode.
-    model.load_state_dict(best_model['weights'])
+    # Import the model architecture from adjacent file
+    # import model_arch_ae09_rmbias as m;
+    import model_arch_ae09 as m
+
+    # Import model weights from .h5 file:
+    dirpath = pathlib.Path(__file__).resolve().parent
+    best_model = load(
+        dirpath.joinpath("model_weights_ae98.h5"),
+        map_location=device(device_str)
+    )
+    model = m.model(best_model).to(device_str)
+
+    # Load weights and set to evaluation mode:
+    model.load_state_dict(best_model["weights"])
     model.eval()
     # print("Model has been loaded with trained weights.")
+
     return model
 
+
 # Compute drag
-
 def compute_reshape_drag(*args):
-    model, X3, X2, Y_out, num_col= args
-    num = 1
-    #while exists(f"from_mima_{num:02d}")==True:
-    #    num+=1
-    #dump({'X3':X3,'X2':X2,'Y_out':Y_out},open(f"from_mima_{num:02d}",'wb'))
-    #X2 = X2.astype(float32)
-    #whichlat=zeros(X2.shape[0],dtype=int)
-    #for j,la in enumerate(model.lat.astype(float32)):
-    #    whichlat[X2[:,0]==la]=j
-    #u_range = array([max(X3[l,0,:model.source_level[whichlat[l]]])-min(X3[l,0,:model.source_level[whichlat[l]]]) for l in range(X3.shape[0])])
-    #del whichlat
-    #i = searchsorted(model.bin_edges, u_range, side='left')
-    #del u_range; 
-    imax=128
-    print(model.lon[:])
-    print(len(model.lon[:]))
-    for j in range(X2.shape[0]//imax):
-        X2[j*imax:(j+1)*imax,1]=model.lon[:]
-    X3 = from_numpy(X3.astype(float32))
-    X2 = from_numpy(X2.astype(float32))
+    model, X3, X2, Y_out, num_col = args
 
+    # Provide output to shell when running MiMA for debugging purposes:
+    # num = 1
+    # while exists(f"from_mima_{num:02d}")==True:
+    #    num+=1
+    # dump({'X3':X3,'X2':X2,'Y_out':Y_out},open(f"from_mima_{num:02d}",'wb'))
+    # X2 = X2.astype(float32)
+    # whichlat=zeros(X2.shape[0],dtype=int)
+    # for j,la in enumerate(model.lat.astype(float32)):
+    #    whichlat[X2[:,0]==la]=j
+    # u_range = array([max(X3[l,0,:model.source_level[whichlat[l]]])-min(X3[l,0,:model.source_level[whichlat[l]]]) for l in range(X3.shape[0])])
+    # del whichlat
+    # i = searchsorted(model.bin_edges, u_range, side='left')
+    # del u_range;
+
+    # Reshape to dimensions required by model and convert to torch tensors:
+    imax = 128
+    for j in range(X2.shape[0] // imax):
+        X2[j * imax: (j + 1) * imax, 1] = model.lon[:]
+    X3 = from_numpy(X3.astype(np.float32))
+    X2 = from_numpy(X2.astype(np.float32))
+
+    # Run model and reshape output into form expected by MiMA:
     with no_grad():
-        temp = model([X3,X2])
-    # temp =temp.double()- model.sm_mean[i,:] # remove mean bias of the corresponding sample urange bin statistic
-    temp = reshape(temp,(num_col,imax,40));
-    temp = transpose(temp, 0, 1);
-    Y_out[:,:,:]=temp.detach().numpy()[:,:,:]
+        temp = model([X3, X2])
+
+    # Remove mean bias of the corresponding sample urange bin statistic
+    # temp =temp.double()- model.sm_mean[i,:]
+
+    temp = reshape(temp, (num_col, imax, 40))
+    temp = transpose(temp, 0, 1)
+    Y_out[:, :, :] = temp.detach().numpy()[:, :, :]
     # del temp, i
+
+    # Provide output to shell when running MiMA for debugging purposes:
     # dump({'Y_out':Y_out},open(f"to_mima_{num:02d}",'wb'))
+
     return Y_out
 
-if __name__ == "__main__":
-    trained_model = initialize()
-    # Yo = zeros(10) + 1.0
-    # X2 = zeros(10) + 1.0
-    # X3 = zeros(10) + 1.0
-    Yo = compute_reshape_drag(trained_model, X3, X2, Y_out, 2)
 
+if __name__ == "__main__":
+    # Short script for standaline testing and debugging from command line
+    trained_model = initialize()
+    X3 = np.asfortranarray(np.random.randn(128 * 2, 4, 40).astype(np.float32))
+    X2 = np.asfortranarray(np.random.randn(128 * 2, 3).astype(np.float32))
+    temp = np.random.randn(128 * 2, 40)
+    Y_out = np.asfortranarray(np.zeros((128, 2, 40)), dtype=np.float64)
+    result = compute_reshape_drag(trained_model, X3, X2, Y_out, 2)
+    print(result)
