2c0fb73104e9dcc6dc48cbb0698abb20596fd6de
lm2612 <lauraman@stanford.edu>
2023-05-15 16:14:09 -0700

    added tolerance option, with default 1e-8. Also added OPTIND=1 to ensure that options are not remembered if the test is repeated

--- tests/single_timestep/run_single_timestep_test.sh
+++ tests/single_timestep/run_single_timestep_test.sh
@@ -3,18 +3,22 @@
 # Minimal runscript for single timestep
 ulimit -s unlimited
 
+OPTIND=1
+
 # MiMA must be compiled and all modules must be loaded before running
 N_PROCS=4
 PATH_TO_CCOMB="../../build/mppnccombine"
 PATH_TO_EXEC="../../build/mima.x"
+TOLERANCE=1e-8
 
 # OPTIONAL VALUES TO CHANGE PATH TO EXECUTABLE ETC
-while getopts p:x:c: flag
+while getopts "p:x:c:t:" flag
 do
     case "${flag}" in
         p) N_PROCS=${OPTARG};;
         x) PATH_TO_EXEC=${OPTARG};;
         c) PATH_TO_CCOMB=${OPTARG};;
+        t) TOLERANCE=${OPTARG};;
     esac
 done
 
@@ -36,8 +40,8 @@ ${PATH_TO_CCOMB} -r atmos_avg.nc atmos_avg.nc.????
 
 echo "Done"
 
-echo "Test is the output the same as the baseline files?"
-cdfdiff atmos_daily.nc  atmos_daily_base.nc 
-cdfdiff atmos_avg.nc  atmos_avg_base.nc
+echo "Test is the output the same as the baseline files, considering tolerance $TOLERANCE ?"
+cdfdiff -t $TOLERANCE,0 atmos_daily.nc  atmos_daily_base.nc 
+cdfdiff -t $TOLERANCE,0 atmos_avg.nc  atmos_avg_base.nc
 
 
